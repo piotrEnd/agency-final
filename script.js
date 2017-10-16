8a745910019665cacc0236dbae25fd2f75107367
piotrEnd <piotrend@gmail.com>
2017-10-16 10:52:31 +0200

    adding jquery reference for gmaps.js; opacity on background of slider2; pin-icon on map

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 // MAP from https://hpneo.github.io/gmaps/
 var map = new GMaps({
 
-  div: '.map',	
-  lat: 40.7751598, 
-  lng: -73.9650649, // sure, that is where
-  zoom: 13	
+	div: '.map',
+	lat: 40.7751598,
+	lng: -73.9650649, // sure, that is where
+	zoom: 13
 });
 
-map.addMarker({		
-  lat: 40.7750613, 
-  lng: -73.966543,
-  title: 'InteractiveAgency NYC',
-  infoWindow: {	
-	  content: '<p>Are you ready to journey down the rabbit hole?</p>'
+map.addMarker({
+	lat: 40.7750613,
+	lng: -73.966543,
+	title: 'InteractiveAgency NYC',
+	icon: { url: ('img/location-pin.png')},
+	infoWindow: {
+		content: '<p>Are you ready to journey down the rabbit hole?</p>'
 	}
 });
+
